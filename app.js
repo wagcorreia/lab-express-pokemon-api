e1c4d1076c43f2f0a054af1a7c43f7cc3e3748cd
wagnao <wagner.correia@gmail.com>
2021-07-12 21:05:38 -0300

    lab bonus 2 fixed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { response } = require('express')
+const { res } = require('express')
 const express = require('express')
 
 const PORT = 4000
@@ -11,10 +11,10 @@ app.use(express.json())
 
 // -- Define your route listeners here! --
 
-app.get('/pokemon', (request, response) => {
+app.get('/pokemon', (req, res) => {
   console.log('A rota /pokemon foi chamada')
 
-  return response.send(allPokemon)
+  return res.send(allPokemon)
 })
 
 app.get('/pokemon/search', (req, res) => {
@@ -41,52 +41,58 @@ app.get('/pokemon/search', (req, res) => {
 app.get('/pokemon/:id', (req, res) => {
   const id = req.params.id
 
-  const foundPokemon = allPokemon.find((pokemon) => {
-    return pokemon.id === Number(id)
+  const foundPokemon = allPokemon.find((pokemonElement) => {
+    return pokemonElement.id === Number(id)
   })
 
-  return response.json(foundPokemon)
+  return res.json(foundPokemon)
 })
 
-app.post("/pokemon", (req, res) => {
+//CRUD
+app.post('/pokemon', (req, res) => {
+  const formData = req.body
 
-    const formData = req.body;
+  const newPokemon = {
+    id: formData.id,
+    name: formData.name,
+    types: formData.types[('', '')],
+    height: formData.height,
+    weight: formData.weight,
+    sprite: formData.sprite,
+  }
 
-    const newContact = {
-        id: formData.id,
-        name: formData.name,
-        types: formData.types["", ""],
-        height: formData.height,
-        weight: formData.weight,
-        sprite: formData.sprite
-      };
+  allPokemon.push(newPokemon)
 
+  return res.json(newPokemon)
+})
 
-app.put("/pokemon/:id", (req, res) => {
-        const formData = req.body;
+app.put('/pokemon/:id', (req, res) => {
+  const formData = req.body
 
-        const id = req.params.id;
+  const id = req.params.id
 
-        const foundContact = allPokemon.find((contactElement) => {
-          return contactElement.id === Number(id);
-        });
-        const index = allPokemon.indexOf(foundContact);
+  const foundPokemon = allPokemon.find((pokemonElement) => {
+    return pokemonElement.id === Number(id)
+  })
+  const index = allPokemon.indexOf(foundPokemon)
 
-        allPokemon[index] = { ...foundContact, ...formData };
+  allPokemon[index] = { ...foundPokemon, ...formData }
+  return res.json(allPokemon[index])
+})
 
-app.delete("/pokemon/:id", (req, res) => {
-            const index = allPokemon.findIndex((contactElement) => {
-              return contactElement.id === req.params.id;
-            });
+app.delete('/pokemon/:id', (req, res) => {
+  const index = allPokemon.findIndex((pokemonElement) => {
+    return pokemonElement.id === req.params.id
+  })
 
-            if (index > 0) {
-                allPokemon.splice(index, 1);
-                return res.json({ msg: "Pokemon deleted successfully" });
-              } else {
-                return res.json({ msg: "Pokemon not found." });
-              }
-            });
+  if (index > 0) {
+    allPokemon.splice(index, 1)
+    return res.json({ msg: 'Pokemon deleted successfully' })
+  } else {
+    return res.json({ msg: 'Pokemon not found.' })
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server up and running at port ${PORT}`)
-});
+})
